976c482071cddd3bbaa13e6eda4b6975c36fb2fe
iberojay <156839787+iberojay@users.noreply.github.com>
2024-02-02 13:14:52 -0700

    Create Circle.java

---
+++ app/src/main/java/com/example/charlie_kim_fan_club/Circle.java
@@ -0,0 +1,14 @@
+package com.example.charlie_kim_fan_club;
+
+public class Circle extends Shape {
+    int x, y;
+    String color = "$BLUE";
+
+    public Circle(int x, int y) {
+        super(x, y);
+    }
+}
+
+
+
+
