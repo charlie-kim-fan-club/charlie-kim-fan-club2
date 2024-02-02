67dadd27d09d7fdf0a64573f1e97b968c289ebad
meekopun <136206108+meekopun@users.noreply.github.com>
2024-02-02 13:05:34 -0700

    RHOMBUS.java
    I accidentally did not put it in the right place

--- app/src/main/java/com/example/charlie_kim_fan_club/Rhombus.java
+++ app/src/main/java/com/example/charlie_kim_fan_club/Rhombus.java
@@ -1,7 +1,9 @@
 package com.example.charlie_kim_fan_club;
 
 public class Rhombus extends Shape{
-    int x,y;
+    int x , y;
+    String color = "$GREEN";
+
     public Rhombus(int x, int y){
         super(x,y);
     }
