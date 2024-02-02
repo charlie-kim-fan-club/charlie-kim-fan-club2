a373dbbc41d77dfa6d24eddfd71e32c19a66b5a8
Karan0Sidhu <kjsidhu@ualberta.ca>
2024-02-02 12:22:42 -0700

    added shape

---
+++ app/src/main/java/com/example/charlie_kim_fan_club/Shape.java
@@ -0,0 +1,10 @@
+package com.example.charlie_kim_fan_club;
+
+public abstract class Shape {
+    int x,y;
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    public Shape(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+}
